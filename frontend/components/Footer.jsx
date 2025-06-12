9e61ce6c2221ec7b046e0c7601676fece5905e29
Régis Daum <rdaum@john-steel.com>
2025-06-12 16:53:13 +0200

    Adjust footer gaps

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Footer() {
    return (
       <footer className="bg-primary">
-         <div className="wrapper py-5 flex flex-wrap items-center justify-between">
-            <nav className="flex gap-4">
+         <div className="wrapper py-5 flex flex-wrap items-center justify-between gap-2">
+            <nav className="flex flex-wrap gap-x-4 gap-y-2">
                <Link href="/mentions-legales">Mentions légales</Link>
                <Link href="/politique-de-confidentialite">
                   Politique de confidentialité
@@ -13,7 +13,7 @@ export default function Footer() {
                <Link href="/cgv">CGV</Link>
             </nav>
             <Link
-               className="flex itels-center gap-2"
+               className="flex itelm-center gap-2"
                href="https://graphandco.com"
                target="_blank"
             >
