40f658ef6f406314f2fd5793d29bdd68ab5a1797
Régis Daum <rdaum@john-steel.com>
2025-12-14 15:59:27 +0100

    Changed remottePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: ["localhost", "admin-o2dentaire.graphandco.com"],
       remotePatterns: [
          {
             protocol: "https",
             hostname: "admin-o2dentaire.graphandco.com",
             pathname: "/uploads/**",
+         },
+         {
+            protocol: "http",
+            hostname: "localhost",
+            pathname: "/**",
          },
       ],
    },
